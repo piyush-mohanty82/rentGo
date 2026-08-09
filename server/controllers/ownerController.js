9e860dd6019c437ee4302d737cc470bdbb8933bf
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-08-09 17:16:22 +0530

    completed backend and linking with frontend

--- server/controllers/ownerController.js
+++ server/controllers/ownerController.js
@@ -1,4 +1,5 @@
 import imagekit from "../configs/imageKit.js";
+import Booking from "../models/Booking.js";
 import Car from "../models/Car.js";
 import User from "../models/User.js";
 import fs from "fs";
@@ -118,7 +119,60 @@ export const getDashboardData = async (req,res) => {
        }
 
        const cars = await Car.find({owner: _id});
+
+       const bookings = await Booking.find({owner: _id}).populate('car').sort({createdAt: -1});
+
+       const pendingBookings = await Booking.find({owner: _id,status:"pending"});
+       const completedBookings = await Booking.find({owner: _id,status:"confirmed"});
+
+       // calculate monthly revenue from bookings where status is confirmed
+       const monthlyRevenue = bookings.slice().filter(booking => booking.status === 'confirmed').reduce((acc,booking) => acc + booking.price,0);
+
+       const dashboardData = {
+        totalCars: cars.length,
+        totalBookings: bookings.length,
+        pendingBookings: pendingBookings.length,
+        completedBookings: completedBookings.length,
+        recentBookings: bookings.slice(0,3),
+        monthlyRevenue
+       }
        
+       res.json({success: true,dashboardData})
+    } catch (error) {
+        console.log(error.message)
+        res.json({success: false,message: error.message})
+    }
+}
+
+// API to update user image
+export const updateUserImage = async (req,res) => {
+    try {
+        const {_id} = req.user;
+        const imageFile = req.file;
+
+        //upload image to imagekit
+        const fileBuffer = fs.readFileSync(imageFile.path);
+        const response = await imagekit.upload({
+            file: fileBuffer,
+            fileName: imageFile.originalname,
+            folder: '/users'
+        })
+
+        // optimization through imagekit URL transformation
+        var optimizedImageURL = imagekit.url({
+            path : response.filePath,
+            transformation : [
+                {width: '400'}, // width resizing
+                {quality: 'auto'}, // Auto compression
+                {format: 'webp'} // Convert to modern format
+            ]
+        });
+
+        const image = optimizedImageURL;
+
+        await User.findByIdAndUpdate(_id,{image})
+        res.json({success: true, message: "Image Updated"})
+
     } catch (error) {
         console.log(error.message)
         res.json({success: false,message: error.message})
--- server/models/Booking.js
+++ server/models/Booking.js
@@ -11,6 +11,6 @@ const bookingSchema = new mongoose.Schema({
     price : {type: Number,required: true}
 },{timestamps: true})
 
-const Booking = mongoose.model('Booking',carSchema)
+const Booking = mongoose.model('Booking',bookingSchema)
 
 export default Booking
