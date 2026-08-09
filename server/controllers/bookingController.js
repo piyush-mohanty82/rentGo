20e15559f9d5f3eac7b56414b648f91bce28bb78
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-08-09 16:41:19 +0530

    completed bookingController

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -117,3 +117,26 @@ export const getOwnerBookings = async (req,res) => {
         res.json({success: false,message: error.message})
     }
 }
+
+// API to change booking status
+export const changeBookingStatus = async (req,res) => {
+    try {
+       const {_id} = req.user;
+       const {bookingId,status} = req.body;
+
+       const booking = await Booking.findById(bookingId);
+
+       if(booking.owner.toString() !== _id.toString()){
+        return res.json({success: false,message: "Unauthorized"})
+       }
+
+       booking.status = status;
+       await booking.save();
+
+       res.json({sucess: true,message: "Status Updated"})
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false,message: error.message})
+    }
+}
