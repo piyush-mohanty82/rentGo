7c648304f8b5e0fd31a018cc8de1555e2903e33e
Piyush Mohanty <piyushmohanty509@gmail.com>
2026-08-11 21:22:32 +0530

    get car controller added

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 import User from "../models/User.js"
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
+import Car from "../models/Car.js";
 
 
 // Generate JWT Token
@@ -69,3 +70,14 @@ export const getUserData = async (req,res) => {
     }
 }
 
+// get all cars for the frontend
+
+export const getCars = async (req,res) => {
+    try {
+        const cars = await Car.find({isAvaliable:true})
+        res.json({success:true,cars})
+    } catch (error) {
+        console.log(error.message);
+        res.json({success: false, message: error.message})
+    }
+}
